b41b8f49cb5639210e87fba29511f22c9c336d70
gracebir <grace.devolop@gmail.com>
2023-11-19 16:12:36 +0200

    translate y

--- src/components/CardImage.jsx
+++ src/components/CardImage.jsx
@@ -6,7 +6,7 @@ const CardImage = ({ id, img, isPic }) => {
     return (
         <div>
             {isPic ? (
-                <div className='rounded-lg w-[325px] h-[325px] shadow-lg'>
+                <div className='rounded-lg hover:translate-y-2 duration-300 w-[325px] h-[325px] shadow-lg'>
                     <img
                         className='w-full h-full rounded-lg'
                         src={img}
@@ -19,7 +19,7 @@ const CardImage = ({ id, img, isPic }) => {
                     width='750'
                     height='500'
                     controls
-                    className=' shadow-lg rounded-lg w-[325px] h-[325px]'
+                    className='hover:translate-y-2 duration-300 shadow-lg rounded-lg w-[325px] h-[325px]'
                 >
                     <source className='rounded-lg' src={img} type='video/mp4' />
                 </video>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -5,12 +5,7 @@ import React from "react";
 const Contact = () => {
     return (
         <div className='flex items-center justify-center p-6'>
-            <div
-                style={{
-                    backgroundImage: `url('../src/assets/contact.webp')`,
-                }}
-                className='w-[237px] bg-no-repeat cursor-pointer bg-cover hover:scale-95 duration-300 h-[237px] lg:w-[325px] lg:h-[325px] rounded-lg'
-            ></div>
+            <div className='w-[237px] goma bg-no-repeat cursor-pointer bg-cover hover:scale-95 duration-300 h-[237px] lg:w-[325px] lg:h-[325px] rounded-lg'></div>
         </div>
     );
 };
