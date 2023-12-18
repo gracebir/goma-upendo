1ea645abfe5d7720a03e64978061c81065febae3
gracebir <grace.devolop@gmail.com>
2023-12-18 10:48:32 +0200

    add new video

--- src/data.js
+++ src/data.js
@@ -2,16 +2,15 @@
 
 import pict1 from "./assets/pict1.jpeg";
 import pict2 from "./assets/pict2.jpeg";
-import pict6 from "./assets/pict6.jpeg";
 import video1 from "./assets/video1.mp4";
-import video4 from "./assets/video4.mp4";
+import video5 from "./assets/video5.mp4";
 import video3 from "./assets/video3.mp4";
 
 export const gallery = [
     {
         id: 1,
         isPic: false,
-        img: video4,
+        img: video5,
     },
     {
         id: 2,
